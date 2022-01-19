2ebe592e1f877e61560e6821ff1727d33145735b
Nguyen Dat <datnvph14562@fpt.edu.vn>
2022-01-19 21:44:35 +0700

    sua lab3

--- src/components/news.js
+++ src/components/news.js
@@ -3,16 +3,17 @@ import data from "../data";
 const News = {
         render() {
             return /* html */ `
-            <h2 class="py-4 font-bold text-4xl ">Tin tức học tập</h2>
+            
+            <h2 class="py-4 font-bold text-2xl uppercase">Tin tức học tập</h2>
             <div class = "news">
                 <div class="flex justify-between gap-6">
                 ${data
                   .map(
                     (post) => `
-                    <div class="col basis-1/3 border-4 p-4">
+                    <div class="col basis-1/3 border-2 p-4">
                         <a href="/news/${post.id}"><img class="w-full " src="${post.img}" alt=""></a>
                         <a href="/news/${post.id}">
-                            <h3 class="py-2 font-bold text-red-700">${post.title}</h3>
+                            <h3 class="py-2 font-bold text-red-500">${post.title}</h3>
                         </a>
                         <p>${post.desc}</p>
                     </div>
@@ -21,16 +22,16 @@ const News = {
                   .join("")}
                     </div>
                     </div>
-        <h2 class="py-4 font-bold text-4xl ">Hoạt động sinh viên </h2>
+        <h2 class="py-4 font-bold text-2xl uppercase">Hoạt động sinh viên</h2>
         <div class="active">
             <div class="flex justify-between gap-6">
         ${data
           .map(
             (post) => `
-            <div class="col basis-1/3 border-4 p-4">
+            <div class="col basis-1/3 border-2 p-4">
                 <a href="/news/${post.id}"><img class="w-full " src="${post.img}" alt=""></a>
                 <a href="/news/${post.id}">
-                    <h3 class="py-2 font-bold text-red-700">${post.title}</h3>
+                    <h3 class="py-2 font-bold text-red-500">${post.title}</h3>
                 </a>
                 <p>${post.desc}</p>
             </div>
